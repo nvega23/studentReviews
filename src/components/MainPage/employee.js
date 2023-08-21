5c6991467ce4c49461c3c0a244f6b4c813fd6efe
Nestor Vega <vega.nestor1@gmail.com>
2023-08-20 20:21:41 -0700

    employee animation

--- src/components/MainPage/employee.js
+++ src/components/MainPage/employee.js
@@ -24,13 +24,16 @@ const Employee = () => {
                     </div>
                 </div>
             </div>
-            {/* <div className='employeeContainer'>
+            <div className='employeeContainer'>
                 <div className='TextContainer'>
                     <div className='aboutMeTextContainer'>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                     </div>
                     <div className='aboutMe'>
-                        Nestor Vega
+                        <div className='nameContainer'>
+                            Nestor Vega
+                            <button onClick={reviewEmployee}>Evaluate Employee</button>
+                        </div>
                     </div>
                 <img className='employeeImage' src="https://images.unsplash.com/photo-1600275669439-14e40452d20b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80"/>
                 </div>
@@ -39,13 +42,16 @@ const Employee = () => {
                 <img className='employeeImage' src="https://images.unsplash.com/photo-1590649880765-91b1956b8276?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"/>
                 <div className='TextContainer'>
                     <div className='aboutMe'>
-                        Nestor Vega
+                        <div className='nameContainer'>
+                            Nestor Vega
+                            <button onClick={reviewEmployee}>Evaluate Employee</button>
+                        </div>
                     </div>
                     <div className='aboutMeTextContainer'>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                     </div>
                 </div>
-            </div> */}
+            </div>
         </>
     )
 }
